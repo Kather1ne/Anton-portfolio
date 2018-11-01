b8a27aa2b17b77668787406b94716564dfb2cd39
K@ther1ne <itskatestime@gmail.com>
2018-11-02 01:42:38 +0300

    add images and new type of scroll

--- js/light-box.js
+++ js/light-box.js
@@ -1,12 +1,17 @@
 $(function() {
 
-	var arr = [];
+	var arr = { '1':3, '2':3, '3':2};
 
 
 	$('.slider-main-block').on('click', function(){
 		var imgName = $(this).find('img').attr('src').match(/\d+/)[0];
 		var i = 0;
 		var namesArr = [];
+<<<<<<< HEAD
+		var mainImgContent = '<img src="img/' + imgName + '-1.jpg">';	
+		$('.big-img').html(mainImgContent);
+		$('.bg, .light-box').removeClass('hide');
+=======
 		$(".thumbnails").load("img/ a[href$='.jpg']", function() {
 			
 			$('.thumbnails').find("a:contains(" + imgName + "-)").each(function () {
@@ -26,16 +31,18 @@ $(function() {
 		// var mainImgContent = '<img src="img/' + arr[0] + '">';	
 		// $('.big-img').html(mainImgContent);
 		// $('.bg, .light-box').removeClass('hide');
+>>>>>>> 0d25c25870dbf06d6f616485785d43a47729b342
 	});
 
 	$('.light-box .arrow.right').on('click', function() {
+		var imgNameFirst = Number($('.light-box img').attr('src').match(/\d/)[0]);
 		var imgName = Number($('.light-box img').attr('src').match(/-\d+/)[0].replace("-",""));
 		var imgNumber = imgName + 1;
-		if (imgNumber <= arr.length) { 
-			$('.light-box img').attr('src', 'img/' + arr[imgNumber - 1]);
+		if (imgNumber <= arr[imgName]) { 
+			$('.light-box img').attr('src', 'img/' + imgNameFirst + '-' + imgNumber + '.jpg');
 		}
 		else {
-			$('.light-box img').attr('src', 'img/' + arr[0]);
+			$('.light-box img').attr('src', 'img/' + imgNameFirst + '-' + '1' + '.jpg');
 		}		
 	}); 
 
